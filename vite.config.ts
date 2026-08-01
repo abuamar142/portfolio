4858394c3438fd340fcf78e1d9c3b9d3d48a3cae
Abu Amar <abuamar.albadawi@gmail.com>
2026-08-01 12:08:38 +0700

    feat: add sitemap generation with vite-plugin-sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Sitemap({
+      dynamicRoutes: ['/blogs'],
+      hostname: 'https://abuamar.online',
+      outDir: 'dist',
+      robots: [{ userAgent: '*', allow: '/' }],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
